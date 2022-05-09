0daa079fa7a27aebe0c4bc6a168c1caec3265b60
luctst <lucas.tostee@gmail.com>
2022-05-09 14:21:36 +0200

    Start create service wedding pages

---
+++ pages/wedding/_service.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="wedding--service">
+    <header-vue></header-vue>
+  </section>
+</template>
+
+<script>
+import globalMixin from '~/mixins/global';
+
+export default {
+  name: 'WeddingService',
+  mixins: [globalMixin],
+  layout: 'default',
+}
+</script>
